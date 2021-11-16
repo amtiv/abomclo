cecff414199f1b3f72e930770300a0810cb6e33a
amtiv <89408740+amtiv@users.noreply.github.com>
2021-11-16 19:39:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
 
-// menuBtn.addEventListener("click", () => {
-//   menuBtn.classList.toggle("active");
-//   navigation.classList.toggle("active");
-// });
-
 const btns = document.querySelectorAll(".nav-btn");
 const slides = document.querySelectorAll(".video-slide");
 const contents = document.querySelectorAll(".content");
